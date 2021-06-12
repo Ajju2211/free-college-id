5dfca72b9a3a1f4dcdfaba791aae7d239349f96f
Ajju2211 <azharuddinm2211@gmail.com>
2021-06-12 17:56:33 +0000

    updated Main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import {Switch, BrowserRouter, Route} from 'react-router-dom'
-import CollegeForm from './components/CollegeForm/CollegeForm'
-import Main from './components/Main/Main'
-import NavBar from "./components/NavBar/NavBar"
+import React from "react";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
+import CollegeForm from "./components/CollegeForm/CollegeForm";
+import Main from "./components/Main/Main";
+import NavBar from "./components/NavBar/NavBar";
 export default function App() {
   return (
-<BrowserRouter>
-  <NavBar/>
-  <Switch>
-    <Route exact path='/' component={Main}/>
-    <Route  path='/college/:id' component={CollegeForm}/>
-  </Switch>
-</BrowserRouter>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/college/:id" component={CollegeForm} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Carousel from './Carousel/Carousel'
+import CollegesBoard from './CollegesBoard/CollegesBoard'
 
 export default function Main() {
     return (
-        <div>
-        Main    
-        </div>
+        <>
+            <Carousel/>
+            <CollegesBoard/>
+        </>
     )
 }
