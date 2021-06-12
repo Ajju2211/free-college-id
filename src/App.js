8935deb54bb26758f50725aae89da8f027ccf0c9
Ajju2211 <azharuddinm2211@gmail.com>
2021-06-12 17:50:29 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import {Switch, BrowserRouter, Route} from 'react-router-dom'
 import CollegeForm from './components/CollegeForm/CollegeForm'
 import Main from './components/Main/Main'
+import NavBar from "./components/NavBar/NavBar"
 export default function App() {
   return (
 <BrowserRouter>
+  <NavBar/>
   <Switch>
     <Route exact path='/' component={Main}/>
     <Route  path='/college/:id' component={CollegeForm}/>
