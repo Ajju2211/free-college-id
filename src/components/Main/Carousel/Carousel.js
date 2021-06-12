2f364d160eb637c5a6745de93b51fbbe9618edd7
Srikar.Rama <srikarrama555@gmail.com>
2021-06-12 21:18:36 +0000

    updated

--- src/components/Main/Carousel/Carousel.js
+++ src/components/Main/Carousel/Carousel.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
-export default function Carousel() {
+import withSizes from 'react-sizes'
+import './Carousel.css'
+const  Carousel =(props)=> {
     return (
-        <div>
-            Carousel
+        <div style={{height:props.WinHeight,position:'relative' ,width:"100%",backgroundColor:"#434343",justifyContent:"center", display:"flex",alignItems:"center"}}>
+            {props.winWidth >1050?<img className="personid" src="Images/person.png"></img>:<></>}
+            
+            <div className="heading" style={{display:"flex",flexDirection:"column"}}>
+            <h1 style={{textAlign:"center"}} >Get your college ID card for free</h1>
+            <p style={{textAlign:"center"}}>Download within few seconds...!</p>
+            <button className="findbutton">Find your college</button>
+            </div>
         </div>
     )
 }
+const mapSizesToProps = ({ height ,width }) => ({
+    WinHeight:height-60,
+    winWidth:width
+  })
+   
+  export default withSizes(mapSizesToProps)(Carousel)
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,9 @@
 import React from 'react'
-
+import './Navbar.css'
 export default function NavBar() {
     return (
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link" href="#">Features</a>
-                        <a className="nav-item nav-link" href="#">Pricing</a>
-                        <a className="nav-item nav-link disabled" href="#">Disabled</a>
-                    </div>
-                </div>
+                <h1 className="logo"><a className="navbar-brand" href="#" style={{color:"white"}}>Free College ID</a></h1>
             </nav>
     )
 }
