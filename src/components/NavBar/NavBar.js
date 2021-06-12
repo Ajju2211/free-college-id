98c955790cb6cf44900a87d9c994d6ca28ead6a9
Srikar.Rama <srikarrama555@gmail.com>
2021-06-12 18:30:54 +0000

    updated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function NavBar() {
     return (
-        <div>
-            NAvabr
-        </div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link" href="#">Features</a>
+                        <a className="nav-item nav-link" href="#">Pricing</a>
+                        <a className="nav-item nav-link disabled" href="#">Disabled</a>
+                    </div>
+                </div>
+            </nav>
     )
 }
