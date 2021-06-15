cde158dcb080714fc0d4a39ecdf960fa78f5d384
Srikar.Rama <srikarrama555@gmail.com>
2021-06-15 15:58:14 +0000

    updated

--- src/components/Main/Carousel/Carousel.js
+++ src/components/Main/Carousel/Carousel.js
@@ -4,7 +4,7 @@ import './Carousel.css'
 const  Carousel =(props)=> {
     return (
         <div style={{height:props.WinHeight,position:'relative' ,width:"100%",backgroundColor:"#434343",justifyContent:"center", display:"flex",alignItems:"center"}}>
-            {props.winWidth >1050?<img className="personid" src="Images/person.png"></img>:<></>}
+            {props.winWidth >1050?<img className="personid" src="Images/ph1.png"></img>:<></>}
             
             <div className="heading" style={{display:"flex",flexDirection:"column"}}>
             <h1 style={{textAlign:"center"}} >Get your college ID card for free</h1>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,9 @@ import './Navbar.css'
 export default function NavBar() {
     return (
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                <h1 className="logo"><a className="navbar-brand" href="#" style={{color:"white"}}>Free College ID</a></h1>
+                <div style={{display:"flex",alignItems:"center"}}>
+                <img style={{height:"70px",width:"auto",margin:"0 0 0 30%"}} src="/Images/free1.png"></img><h1 className="logo"><a className="navbar-brand" href="/" style={{color:"white"}}>College ID Card</a></h1>
+                </div>
             </nav>
     )
 }
