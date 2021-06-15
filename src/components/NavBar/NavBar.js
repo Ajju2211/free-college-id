cc1dfd684a5310293e27b335f23f3f21b136d808
Srikar.Rama <srikarrama555@gmail.com>
2021-06-15 16:10:12 +0000

    update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Navbar.css'
 export default function NavBar() {
     return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <nav className="p-0 navbar navbar-expand-lg navbar-light bg-dark">
                 <div style={{display:"flex",alignItems:"center"}}>
                 <img style={{height:"70px",width:"auto",margin:"0 0 0 30%"}} src="/Images/free1.png"></img><h1 className="logo"><a className="navbar-brand" href="/" style={{color:"white"}}>College ID Card</a></h1>
                 </div>
