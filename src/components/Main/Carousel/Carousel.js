42bae2bc8ff564b25f12dc00acaffa2eefe34924
Srikar.Rama <srikarrama555@gmail.com>
2021-06-15 16:08:43 +0000

    update

--- src/components/Main/Carousel/Carousel.js
+++ src/components/Main/Carousel/Carousel.js
@@ -3,8 +3,8 @@ import withSizes from 'react-sizes'
 import './Carousel.css'
 const  Carousel =(props)=> {
     return (
-        <div style={{height:props.WinHeight,position:'relative' ,width:"100%",backgroundColor:"#434343",justifyContent:"center", display:"flex",alignItems:"center"}}>
-            {props.winWidth >1050?<img className="personid" src="Images/ph1.png"></img>:<></>}
+        <div style={{height:props.WinHeight,width:"100%",backgroundColor:"#434343",justifyContent:"center", display:"flex",alignItems:"center"}}>
+            {props.winWidth >1130?<img className="personid" src="Images/ph1.png"></img>:<></>}
             
             <div className="heading" style={{display:"flex",flexDirection:"column"}}>
             <h1 style={{textAlign:"center"}} >Get your college ID card for free</h1>
