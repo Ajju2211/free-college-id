7528610de0cd8ef2fd9d1d7770b0d632c9bd8bb4
Srikar.Rama <srikarrama555@gmail.com>
2021-06-15 17:21:36 +0000

    updated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,8 @@ export default function NavBar() {
     return (
             <nav className="p-0 navbar navbar-expand-lg navbar-light bg-dark">
                 <div style={{display:"flex",alignItems:"center"}}>
-                <img style={{height:"70px",width:"auto",margin:"0 0 0 30%"}} src="/Images/free1.png"></img><h1 className="logo"><a className="navbar-brand" href="/" style={{color:"white"}}>College ID Card</a></h1>
+                <img src="/Images/free1.png" style={{height:"70px",marginLeft:"20%"}}></img>
+                <h1 className="logo"><a className="navbar-brand" href="/" style={{color:"white"}}>College ID Card</a></h1>
                 </div>
             </nav>
     )
