be27568272c553c1b953414de44aaa3cbef71166
Srikar.Rama <srikarrama555@gmail.com>
2021-06-15 19:13:56 +0000

    updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./footer.css"
+import {VscGithub} from 'react-icons/vsc'
 function Footer() {
     return (
         <div>
             <div style={{ background: "rgb(23 22 22)", position: "relative" }}>
                 <div class="love" style={{ textAlign: "center", padding: "10px 0", color: "black", color: "gray" }}>Made with ❤️ by <span style={{ color: "white", fontWeight: '600' }}>M</span>ohammed <span style={{ color: "white", fontWeight: "600" }}>A</span>zharuddin , <span style={{ color: "white", fontWeight: "600" }}>R</span>ama <span style={{ color: "white", fontWeight: "600" }}>S</span>rikar</div>
                 <div style={{display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"}}>
-                <div class="row" style={{color:"gray",display:"flex",alignItems:"center"}}>
-                <img src="/Images/githubimg.png" style={{ width: "auto", height: "30px"}}></img>Github profiles
+                <div style={{display:"flex",alignItems:"center",color:'gray',fontWeight:"400"}}>
+                <VscGithub style={{fontSize:"1.5em",fontWeight:"400",marginRight:"5px",color:"white"}} /><span>Github profiles</span>
                 </div>
                     <div class="github"><a  href="https://github.com/srikar-rama" style={{ textDecoration: 'none'}}>Rama Srikar</a></div>
                     <div class="github"><a  href="https://github.com/Ajju2211" style={{ textDecoration: 'none' }}>Mohammed Azharuddin</a></div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar() {
     return (
             <nav className="p-0 navbar navbar-expand-lg navbar-light bg-dark">
                 <div style={{display:"flex",alignItems:"center"}}>
-                <img src="/Images/free1.png" style={{height:"70px",marginLeft:"20%"}}></img>
+                <img className="logo1" src="/Images/free1.png" style={{height:"70px"}}></img>
                 <h1 className="logo"><a className="navbar-brand" href="/" style={{color:"white"}}>College ID Card</a></h1>
                 </div>
             </nav>
