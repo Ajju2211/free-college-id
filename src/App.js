a29caffe051f7f6ae89300095a3d10c4d0b7ec0b
Srikar.Rama <srikarrama555@gmail.com>
2021-06-16 09:04:19 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./components/Main/Main";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import "./App.css"
+import Errorpage from "./components/Error/Errorpage";
 export default function App() {
   return (
     <BrowserRouter>
@@ -12,6 +13,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route path="/college/:id" component={CollegeForm} />
+        <Route component={Errorpage} />
       </Switch>
       <Footer/>
     </BrowserRouter>
