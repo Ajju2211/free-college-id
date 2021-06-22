c538080717b5d7ccd1b1ae3f08910ceb279e3547
Mohammad Azharuddin <43109538+Ajju2211@users.noreply.github.com>
2021-06-22 23:41:44 +0530

    Update CollegeForm.js

--- src/components/CollegeForm/CollegeForm.js
+++ src/components/CollegeForm/CollegeForm.js
@@ -72,7 +72,7 @@ if(authRequired && !isLogged){
         <div className={styles.signInBox+" card p-0"} style={{width:"50%"}}>
         <h2 className="card-header text-center">Sign in</h2>
         <div className="card-body" style={{display: "flex",flexDirection: "column",alignItems: "center",justifyContent: "center"}}>
-        <p>College email does not open in mobile inbuilt browser (chrome mobile), use either external browser or desktop to signin.</p>
+        <p>College email does not open in few inbuilt browsers of older versions of mobile, use either external browser or desktop to signin is such issues.</p>
         <GoogleLogin
           clientId={clientId}
           hostedDomain="sreenidhi.edu.in"
