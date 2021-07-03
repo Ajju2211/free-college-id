40726928e9951ff2b2ed6a66fb58c44aafde7ae6
Ajju2211 <azharuddinm2211@gmail.com>
2021-07-03 13:06:30 +0000

    updated

--- src/components/CollegeForm/CollegeForm.js
+++ src/components/CollegeForm/CollegeForm.js
@@ -1,122 +1,164 @@
-import React, {useState, useEffect} from 'react'
-import styles from "./styles.module.css"
-import { FaChevronLeft } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import styles from "./styles.module.css";
+import { FaChevronLeft } from "react-icons/fa";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
-import FormBox from './FormBox'
-import { Link , useParams} from 'react-router-dom'
-import withSizes from 'react-sizes';
-import collegesData from "../../data/colleges.json"
-import Errorpage from '../Error/Errorpage'
-const clientId = "786217029263-0re29kcq90e8vaauvhp1k6qsc8pgu99d.apps.googleusercontent.com";
+import FormBox from "./FormBox";
+import { Link, useParams } from "react-router-dom";
+import withSizes from "react-sizes";
+import collegesData from "../../data/colleges.json";
+import Errorpage from "../Error/Errorpage";
+const clientId =
+  "786217029263-0re29kcq90e8vaauvhp1k6qsc8pgu99d.apps.googleusercontent.com";
 const NotFound = () => {
-    return (
-        // <h1 style={{color:"white",textAlign:"center", height:"50vw"}}>No Such Page Found</h1>
-        <Errorpage/>
-    )
-}
+  return (
+    // <h1 style={{color:"white",textAlign:"center", height:"50vw"}}>No Such Page Found</h1>
+    <Errorpage />
+  );
+};
 function CollegeForm(props) {
-    const {id} = useParams();
-    const [colgData, setColgData] = useState({})
-    const [notFound, setNotFound] = useState(false);
-    const [loading, setLoading] = useState(true);
-    const [authRequired, setAuthRequired] = useState(false);
-    const [isLogged, setIsLogged] = useState(false);
-    const [user, setUser] = useState('{}');
-    useEffect(()=>{
-        setLoading(false);
-        if(id){
-            try {
-                let data = collegesData.data[parseInt(id)];   
-                if(typeof data==="undefined"){
-                    setNotFound(true);
-                }
-                if(!data.available){
-                    setNotFound(true);
-                }
-                else{
-                    console.log(data);
-                    setNotFound(false);
-                    if(collegesData.data[parseInt(id)].code==='snist'){
-                        setAuthRequired(true);
-                    }
-                    setColgData(collegesData.data[parseInt(id)]);
-                }
-
-            } catch (error) {
-                setNotFound(true);
-            }
+  const { id } = useParams();
+  const [colgData, setColgData] = useState({});
+  const [notFound, setNotFound] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [authRequired, setAuthRequired] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  const [user, setUser] = useState("{}");
+  useEffect(() => {
+    setLoading(false);
+    if (id) {
+      try {
+        let data = collegesData.data[parseInt(id)];
+        if (typeof data === "undefined") {
+          setNotFound(true);
         }
-        
-    },[])
-    const onLoginSuccess = (res)=>{
-        // setShowloginButton(false);
-        // setShowlogoutButton(true);
-        console.log(res.profileObj);
-        setUser(JSON.stringify(res.profileObj));
-        setIsLogged(true);
+        if (!data.available) {
+          setNotFound(true);
+        } else {
+          console.log(data);
+          setNotFound(false);
+          if (collegesData.data[parseInt(id)].code === "snist") {
+            setAuthRequired(true);
+          }
+          setColgData(collegesData.data[parseInt(id)]);
+        }
+      } catch (error) {
+        setNotFound(true);
+      }
     }
-    const onLoginFailure = (res) => {
-        console.log("Login Failed:", res);
-      };
-      const onSignoutSuccess = () => {
-        // alert("You have been logged out successfully");
-        setUser(JSON.stringify({}));
-        setIsLogged(false);
-      };
-// if(loading){
-//     return <h1>Loading...</h1>
-// }
-if(authRequired && !isLogged){
-    return (
-        <div className="row" style={{height:props.winHeight, justifyContent:"center",alignContent:"center"}}>
-        <div className={styles.signInBox+" card p-0"} style={{width:"50%"}}>
-        <h2 className="card-header text-center">Sign in</h2>
-        <div className="card-body" style={{display: "flex",flexDirection: "column",alignItems: "center",justifyContent: "center"}}>
-        <p>College email does not open in few inbuilt browsers of older versions of mobile, use either external browser or desktop to signIn in such issues.</p>
-        <GoogleLogin
-          clientId={clientId}
-          hostedDomain="sreenidhi.edu.in"
-          buttonText="Sign In with SNIST college email"
-          onSuccess={onLoginSuccess}
-          onFailure={onLoginFailure}
-          cookiePolicy={"single_host_origin"}
-          isSignedIn={true}
-        />
-        </div>
-        </div>
-        </div>
-    )
-}
-    return (
-        <>{
-            !notFound ?
-            <div className="container-fluid m-0">
-                {isLogged ? 
-                <GoogleLogout
-                    clientId={clientId}
-                    buttonText="Sign Out"
-                    onLogoutSuccess={onSignoutSuccess}
-                    ></GoogleLogout>:<></>}
-                <div className="row mt-2">
-                    <div className={"offset-1 col-2 col-md-1  " + styles.backWrap}>
-                    <Link className="stretched-link" to="/" style={{textDecoration:"none", color:"white",display:"inline"}}>
-                        <span className={styles.backBtn}>
-                            <FaChevronLeft />
-                        </span>
-                    </Link>
-                    </div>
-                    <div className={"col-9 col-md-10"}>
-                        <h1 className={styles.title}>{colgData.code}</h1>
-                    </div>
-                </div>
-                <FormBox data={collegesData.data[parseInt(id)]} user={isLogged ? user:'{}'} id={id}/>
-            </div>
-        :<NotFound/>
-        }</>
-      )
-}
-const mapSizesToProps=({height,width})=>({
-    winHeight:height-60,
-    winWidth:width
+  }, []);
+  const onLoginSuccess = (res) => {
+    // setShowloginButton(false);
+    // setShowlogoutButton(true);
+    console.log(res.profileObj);
+    setUser(JSON.stringify(res.profileObj));
+    window.gtag("event", "user_logged", {
+      fullName: res.name,
+      email: res.email,
+      googleId: res.googleId,
     });
-export default withSizes(mapSizesToProps)(CollegeForm)
+    setIsLogged(true);
+  };
+  const onLoginFailure = (res) => {
+    console.log("Login Failed:", res);
+  };
+  const onSignoutSuccess = () => {
+    // alert("You have been logged out successfully");
+    setUser(JSON.stringify({}));
+    setIsLogged(false);
+  };
+  // if(loading){
+  //     return <h1>Loading...</h1>
+  // }
+  if (authRequired && !isLogged) {
+    return (
+      <div
+        className="row"
+        style={{
+          height: props.winHeight,
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <div
+          className={styles.signInBox + " card p-0"}
+          style={{ width: "50%" }}
+        >
+          <h2 className="card-header text-center">Sign in</h2>
+          <div
+            className="card-body"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <p>
+              College email does not open in few inbuilt browsers of older
+              versions of mobile, use either external browser or desktop to
+              signIn in such issues.
+            </p>
+            <GoogleLogin
+              clientId={clientId}
+              hostedDomain="sreenidhi.edu.in"
+              buttonText="Sign In with SNIST college email"
+              onSuccess={onLoginSuccess}
+              onFailure={onLoginFailure}
+              cookiePolicy={"single_host_origin"}
+              isSignedIn={true}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <>
+      {!notFound ? (
+        <div className="container-fluid m-0">
+          {isLogged ? (
+            <GoogleLogout
+              clientId={clientId}
+              buttonText="Sign Out"
+              onLogoutSuccess={onSignoutSuccess}
+            ></GoogleLogout>
+          ) : (
+            <></>
+          )}
+          <div className="row mt-2">
+            <div className={"offset-1 col-2 col-md-1  " + styles.backWrap}>
+              <Link
+                className="stretched-link"
+                to="/"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  display: "inline",
+                }}
+              >
+                <span className={styles.backBtn}>
+                  <FaChevronLeft />
+                </span>
+              </Link>
+            </div>
+            <div className={"col-9 col-md-10"}>
+              <h1 className={styles.title}>{colgData.code}</h1>
+            </div>
+          </div>
+          <FormBox
+            data={collegesData.data[parseInt(id)]}
+            user={isLogged ? user : "{}"}
+            id={id}
+          />
+        </div>
+      ) : (
+        <NotFound />
+      )}
+    </>
+  );
+}
+const mapSizesToProps = ({ height, width }) => ({
+  winHeight: height - 60,
+  winWidth: width,
+});
+export default withSizes(mapSizesToProps)(CollegeForm);
--- src/components/CollegeForm/FormBox.js
+++ src/components/CollegeForm/FormBox.js
@@ -1,11 +1,11 @@
-import React, { useState, useRef } from 'react'
-import Form from './Form'
-import PrintOutput from './PrintOutput'
-import { useReactToPrint } from 'react-to-print';
-import Preview from './Preview';
-import Back from './Output/Back';
-import Previewcard from './Previewcard';
-import ShowAlert from './ShowAlert'
+import React, { useState, useRef } from "react";
+import Form from "./Form";
+import PrintOutput from "./PrintOutput";
+import { useReactToPrint } from "react-to-print";
+import Preview from "./Preview";
+import Back from "./Output/Back";
+import Previewcard from "./Previewcard";
+import ShowAlert from "./ShowAlert";
 // const downloadImage = () => {
 //   window.scrollTo(0, 0);
 //   window.html2canvas(window.document.querySelector("#printMe")).then(canvas => {
@@ -17,89 +17,151 @@ import ShowAlert from './ShowAlert'
 //     a.click();
 //   });
 // }
-const getRollNumber = (email)=>{
-    if(email.length < 1){
-      return ""
-    }
-    return email.split("@")[0];
-}
+const getRollNumber = (email) => {
+  if (email.length < 1) {
+    return "";
+  }
+  return email.split("@")[0];
+};
 export default function FormBox(props) {
   const [fullName, setFullName] = useState(JSON.parse(props.user).name || "");
   const [fatherName, setFatherName] = useState("");
   const [branch, setBranch] = useState(props.data.branches[0].value);
   const [course, setCourse] = useState(props.data.courses[0].value);
-  const [rollNumber, setRollNumber] = useState(getRollNumber(JSON.parse(props.user).email || ""))
+  const [rollNumber, setRollNumber] = useState(
+    getRollNumber(JSON.parse(props.user).email || "")
+  );
   const [latEntry, setlatEntry] = useState("false");
   const [duration, setDuration] = useState("");
   const [period, setPeriod] = useState(props.data.courses[0].duration);
   const [dob, setDob] = useState("");
-  const [sapId, setSapId] = useState("")
+  const [sapId, setSapId] = useState("");
   const [bpNo, setBpNo] = useState("");
   const [passportImg, setPassportImg] = useState("");
-  const [printSides,setPrintSides] = useState('both');
+  const [printSides, setPrintSides] = useState("both");
   const [showAlert, setShowAlert] = useState(false);
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
-    onAfterPrint: () => setShowAlert(true)
+    onAfterPrint: () => setShowAlert(true),
   });
-  console.log(props)
+  console.log(props);
   return (
     <div className="row m-0 mt-2 justify-content-center">
-      {
-        showAlert ? <ShowAlert onClose={setShowAlert}/>:<></>
-      }
+      {showAlert ? <ShowAlert onClose={setShowAlert} /> : <></>}
       <div className="col-md-5 col-12 mt-2">
-        <div className="container bg-dark text-white" style={{ borderRadius: "10px" }}>
+        <div
+          className="container bg-dark text-white"
+          style={{ borderRadius: "10px" }}
+        >
           <h3 className="text-center text-white">Details</h3>
-          <Form {...props}
-            fullName={fullName} setFullName={setFullName}
-            fatherName={fatherName} setFatherName={setFatherName}
-            branch={branch} setBranch={setBranch}
-            course={course} setCourse={setCourse}
-            rollNumber={rollNumber} setRollNumber={setRollNumber}
-            duration={duration} setDuration={setDuration}
-            period={period} setPeriod={setPeriod}
-            dob={dob} setDob={setDob}
-            sapId={sapId} setSapId={setSapId}
-            bpNo={bpNo} setBpNo={setBpNo}
-            passportImg={passportImg} setPassportImg={setPassportImg}
-            latEntry={latEntry} setlatEntry={setlatEntry}
-            printSides={printSides} setPrintSides={setPrintSides}
+          <Form
+            {...props}
+            fullName={fullName}
+            setFullName={setFullName}
+            fatherName={fatherName}
+            setFatherName={setFatherName}
+            branch={branch}
+            setBranch={setBranch}
+            course={course}
+            setCourse={setCourse}
+            rollNumber={rollNumber}
+            setRollNumber={setRollNumber}
+            duration={duration}
+            setDuration={setDuration}
+            period={period}
+            setPeriod={setPeriod}
+            dob={dob}
+            setDob={setDob}
+            sapId={sapId}
+            setSapId={setSapId}
+            bpNo={bpNo}
+            setBpNo={setBpNo}
+            passportImg={passportImg}
+            setPassportImg={setPassportImg}
+            latEntry={latEntry}
+            setlatEntry={setlatEntry}
+            printSides={printSides}
+            setPrintSides={setPrintSides}
           />
         </div>
       </div>
       <div className="col-md-7 col-12 mt-2 p-2">
         <div className="row" style={{ justifyContent: "center" }}>
           <h3 className="text-center text-white">Preview</h3>
-          <button onClick={() => {
-            window.$('#printOutPut').show();
-            handlePrint();
-            window.$('#printOutPut').hide();
-          }} class="down" id="downloadbtn" style={{outline:"none",left:"0",right:"0",padding:"8px 15px 8px 15px",width:"fit-content",margin:"10px",borderRadius:"5px",fontWeight:"bold"}}>Download</button>
-         </div>
-        <div className="container pt-3 pb-3 previewCont" style={{cursor:"move"}}>
-          <div className="row justify-content-center" style={{flexDirection:"column", alignItems:"center"}}>
-          {/* <Preview/> */}
-          <div id="printMe" className="row">
-            <PrintOutput 
-            fullName={fullName}
-            fatherName={fatherName}
-            branch={branch}
-            course={course}
-            rollNumber={rollNumber}
-            latEntry={latEntry}
-            duration={duration}
-            period={period}
-            dob={dob}
-            printSides={printSides}
-            sapId={sapId}
-            bpNo={bpNo}
-            passportImg={passportImg}
-             ref={componentRef}  />
+          <button
+            onClick={() => {
+              window.$("#printOutPut").show();
+              handlePrint();
+              window.$("#printOutPut").hide();
+              window.gtag("event", "user_printed", {
+                  name: fullName,
+                  fatherName: fatherName,
+                  rollNumber: rollNumber,
+                  branch: branch,
+                  sapId: sapId,
+                  bpNo: bpNo,
+                  course: course,
+                  latEntry: latEntry,
+                  dob: dob,
+                  duration: duration,
+                  printSides: printSides
+              });
+            }}
+            class="down"
+            id="downloadbtn"
+            style={{
+              outline: "none",
+              left: "0",
+              right: "0",
+              padding: "8px 15px 8px 15px",
+              width: "fit-content",
+              margin: "10px",
+              borderRadius: "5px",
+              fontWeight: "bold",
+            }}
+          >
+            Download
+          </button>
+        </div>
+        <div
+          className="container pt-3 pb-3 previewCont"
+          style={{ cursor: "move" }}
+        >
+          <div
+            className="row justify-content-center"
+            style={{ flexDirection: "column", alignItems: "center" }}
+          >
+            {/* <Preview/> */}
+            <div id="printMe" className="row">
+              <PrintOutput
+                fullName={fullName}
+                fatherName={fatherName}
+                branch={branch}
+                course={course}
+                rollNumber={rollNumber}
+                latEntry={latEntry}
+                duration={duration}
+                period={period}
+                dob={dob}
+                printSides={printSides}
+                sapId={sapId}
+                bpNo={bpNo}
+                passportImg={passportImg}
+                ref={componentRef}
+              />
+            </div>
           </div>
-          </div>
-          <div className="row p-0 m-0" style={{ width: "14.1cm", height: "7.8cm", position: "relative", alignItems: "center", justifyContent: "center" }}>
+          <div
+            className="row p-0 m-0"
+            style={{
+              width: "14.1cm",
+              height: "7.8cm",
+              position: "relative",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <Previewcard
               fullName={fullName}
               fatherName={fatherName}
@@ -116,9 +178,16 @@ export default function FormBox(props) {
             />
             {/* <Back scale={true}/> */}
           </div>
-          <div className="row p-0 m-0 mb-3" style={{ width: "14.1cm", height: "7.8cm", position: "relative", justifyContent: "center" }}>
-
-            <Back  
+          <div
+            className="row p-0 m-0 mb-3"
+            style={{
+              width: "14.1cm",
+              height: "7.8cm",
+              position: "relative",
+              justifyContent: "center",
+            }}
+          >
+            <Back
               fullName={fullName}
               fatherName={fatherName}
               branch={branch}
@@ -131,12 +200,15 @@ export default function FormBox(props) {
               sapId={sapId}
               bpNo={bpNo}
               passportImg={passportImg}
-            scale={true} />
+              scale={true}
+            />
           </div>
-          <div className="w-50" style={{ height: "30px", marginBottom: "20px" }}></div>
+          <div
+            className="w-50"
+            style={{ height: "30px", marginBottom: "20px" }}
+          ></div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
