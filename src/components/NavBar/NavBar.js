d2981e320e83a1cb47307881679c1f09264fc076
Mohammad Azharuddin <43109538+Ajju2211@users.noreply.github.com>
2022-08-02 10:07:34 +0530

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,6 +7,7 @@ export default function NavBar() {
             <div style={{ display: "flex", alignItems: "center" }}>
                 <img className="logo1" src="/Images/free1.png" style={{ height: "70px" }}></img>
                 <h1 className="logo"><a className="navbar-brand" href="/" style={{ color: "white" }}>College ID Card</a></h1>
+                <a href="https://github.com/Ajju2211/free-college-id" target="_blank" style={{ position: "absolute", right: "2%", color: "white", fontSize: "2em" }}><VscGithub /></a>
                 {/* <a href="https://github.com/Ajju2211/free-college-id" target="_blank" style={{ position: "absolute", right: "2%", color: "white", fontSize: "2em" }}><VscGithub /></a> */}
             </div>
         </nav>
